21ac328028eac54b3e3c147bbf24585bbdce16b0
tiruka <tiruka.engineer@gmail.com>
2024-06-04 10:11:07 +0900

    add tests for rust module

--- src/fib_calcs/fib_numbers.rs
+++ src/fib_calcs/fib_numbers.rs
@@ -4,3 +4,13 @@ use pyo3::prelude::pyfunction;
 pub fn fibonacci_numbers(numbers: Vec<i32>) -> Vec<u64> {
     numbers.iter().map(|&n| fibonacci_number(n)).collect()
 }
+
+#[cfg(test)]
+mod fibonacci_numbers_tests {
+    use super::fibonacci_numbers;
+
+    #[test]
+    fn test_run() {
+        assert_eq!(fibonacci_numbers(vec![1, 2, 3, 20]), vec![1, 1, 2, 6765]);
+    }
+}
